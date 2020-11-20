5f77bcfbb42b80c74b100a5851e1c53373c0befa
khill <kalisha.hill@gmail.com>
2020-11-20 12:25:14 -0600

    added logic to finish the game

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const diceEl = document.querySelector('.dice');
 
 // Elements for the buttons
 const roll = document.querySelector('.btn--roll');
-const hold = document.querySelector('.bnt--hold');
+const hold = document.querySelector('.btn--hold');
 const newGame = document.querySelector('.btn--new');
 
 // starting conditions
@@ -31,31 +31,72 @@ const score = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+// Variable to set the state of the game (whether or not we are still playing)
+let playing = true;
+
+// function to switch the player
+function switchPlayer() {
+  // Set the current score to 0 and display it
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  // Switch the player
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  // Change the background color based on the active player
+  player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');
+}
+
 // Function for user rolling the dice
 roll.addEventListener('click', function () {
-  // 1. Random number generator for the dice
-  let dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  // If playing is set to true, then you can play the game
+  if (playing) {
+    // 1. Random number generator for the dice
+    let dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
-  // 2. display the dice by removing the hidden class
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    // 2. display the dice by removing the hidden class
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. Check if it is a one or not
-  if (dice !== 1) {
-    // add the dice to the current score
-    currentScore += dice;
-    document.getElementById(
-      `current--${activePlayer}`
-    ).textContent = currentScore;
+    // 3. Check if it is a one or not
+    if (dice !== 1) {
+      // add the dice to the current score
+      currentScore += dice;
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
 
-    // NOTE - change later based on the current player
-  } else {
-    // Current score is 0 and switch to the next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0.classList.toggle('player--active');
-    player1.classList.toggle('player--active');
+      // NOTE - change later based on the current player
+    } else {
+      // Current score is 0 and switch to the next player
+
+      switchPlayer();
+    }
+  }
+});
+
+// Adding an event handler for holding the score:
+hold.addEventListener('click', function () {
+  if (playing) {
+    // 1. add the score to the active players overall score
+    score[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+
+    // 2. check to see if the score is 100
+    if (score[activePlayer] >= 20) {
+      // finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // 3. switch to the other player of score hasn't reached 100
+      switchPlayer();
+    }
   }
 });
